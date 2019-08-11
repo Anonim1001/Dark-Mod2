146e837fcec2c9f966ac4f301f9a9704cb04ede2
Anonim2019 <53478580+Anonim1001@users.noreply.github.com>
2019-08-11 12:50:01 +0700

    Add files via upload

---
+++ README.py
@@ -0,0 +1,13 @@
+# Dark-Mod2
+
+
+$ pkg upgrade && pkg update 
+$ pkg install python2
+$ pip2 install requests
+$ pip2 install mechanize
+$ pkg install git
+$ git clone https://github.com/Anonim1001/Dark-Mod2
+$ ls
+$ cd Dark-Mod2
+$ ls
+$ python2 Dark-Mod2.py
